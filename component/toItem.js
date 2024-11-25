a4adeed798edbbd37cdb91486693779513908aaa
Ally Juma Said <mahadsaid95@icloud.com>
2024-11-24 17:08:44 -0700

    error when changing opacoty

--- component/toItem.js
+++ component/toItem.js
@@ -10,7 +10,14 @@ export default function TodoItem ({item , pressHandler}){
 
     // keya is the key of the item
     // pressHandler is the function that will be called when the item is pressed
-    
+    // item is the item that will be displayed
+   
+    // code 
+     <TouchableOpacity onPress={() => pressHandler(item.keya)}>
+        <Text style={styles.item}>{item.text}</Text>
+    </TouchableOpacity>
+    // the item will be displayed in a TouchableOpacity component
+    // when the item is pressed, the pressHandler function will be called with the key of the item as a parameter
 }
 
 
